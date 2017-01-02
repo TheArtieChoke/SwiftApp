0137790dbb252e8de2732c154d3c112b91f5cbc3
Patrick Tripodi <pjtripodi@me.com>
2017-01-02 23:11:33 +1100

    Last

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,40 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     // var tapCount = 0
-   
+    
+    //var life :Bool = true
+    
+    
+    
     @IBOutlet weak var textOne: UITextField!
     
     @IBOutlet weak var textTwo: UITextField!
     
     @IBAction func buttonPressed(_ sender: Any) {
         
-        theLabel.text = " \(Double(textOne.text!)! * Double(textTwo.text!)!)"
+        theLabel.text = "\(Double(textOne.text!)! * Double(textTwo.text!)!)"
         
     }
     
     @IBAction func buttonDown(_ sender: Any) {
         
         theLabel.text = "Buttons are cool!"
-         // print("Working!")
+        // print("Working!")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-       
-    
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
